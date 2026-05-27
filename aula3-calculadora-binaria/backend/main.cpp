5c0c047ea38a76a88d8da8921bf0b6144ca9df24
RodrigoGalvezLima <rodrigogalvezlima@usp.br>
2026-05-27 13:51:09 -0300

    feat: desafio da calculadora com serial

--- aula3-calculadora-binaria/backend/main.cpp
+++ aula3-calculadora-binaria/backend/main.cpp
@@ -1,51 +1,72 @@
+#include <Arduino.h>
 
-#include <WebServer.h>
-#include <WiFi.h>
-
-const int LEDS[] = {12,13,14,15};
-
-WebServer server(80);
-
-void handleCalc(){
-    if(server.hasArg("a") && server.hasArg("b") && server.hasArg("op")){
-
-        String paramA = server.arg("a");
-        String paramB = server.arg("b");
-        String op = server.arg("op");
-
-        int valueA = strtol(paramA.c_str(), NULL, 2);
-        int valueB = strtol(paramB.c_str(), NULL, 2);
-
-        int result = (op == "add") ? (valueA + valueB) : (valueA - valueB);
-        result = result & 0x0F;
-        
-        digitalWrite(LEDS[0], result & 0x01);
-        digitalWrite(LEDS[1], (result >> 1) & 0x01); 
-        digitalWrite(LEDS[2], (result >> 2) & 0x01); 
-        digitalWrite(LEDS[3], (result >> 3) & 0x01);
-
-        server.send(200, "text/plain", "OK");
-    } else {
-        server.send(400, "text/plain", "Faltam parâmetros");
-    }
-}
+const int PINOS_LEDS[] = {12, 13, 14, 15};
 
 void setup() {
-pinMode(LEDS[0], OUTPUT);
-pinMode(LEDS[1], OUTPUT);
-pinMode(LEDS[2], OUTPUT);
-pinMode(LEDS[3], OUTPUT);
-digitalWrite(LEDS[0], LOW);
-digitalWrite(LEDS[1], LOW);
-digitalWrite(LEDS[2], LOW);
-digitalWrite(LEDS[3], LOW);
+    pinMode(PINOS_LEDS[0], OUTPUT);
+    pinMode(PINOS_LEDS[1], OUTPUT);
+    pinMode(PINOS_LEDS[2], OUTPUT);
+    pinMode(PINOS_LEDS[3], OUTPUT);
+    
+    digitalWrite(PINOS_LEDS[0], LOW);
+    digitalWrite(PINOS_LEDS[1], LOW);
+    digitalWrite(PINOS_LEDS[2], LOW);
+    digitalWrite(PINOS_LEDS[3], LOW);
 
-WiFi.softAO("Calculadora_ESP32");
-
-server.on("/calc", handleCalc);
-server.begin();
+    Serial.begin(115200);
 }
 
 void loop() {
-server.handleClient();
+    if (Serial.available() > 0) {
+        
+        String entrada = Serial.readStringUntil('\n');
+        entrada.trim();
+
+        int posicaoPrimeiraVirgula = entrada.indexOf(',');
+        int posicaoSegundaVirgula  = entrada.lastIndexOf(',');
+
+        if (posicaoPrimeiraVirgula != -1 && posicaoSegundaVirgula != -1 && posicaoPrimeiraVirgula != posicaoSegundaVirgula) {
+            
+            String stringA = entrada.substring(0, posicaoPrimeiraVirgula);
+            String stringB = entrada.substring(posicaoPrimeiraVirgula + 1, posicaoSegundaVirgula);
+            String operacao = entrada.substring(posicaoSegundaVirgula + 1);
+
+            int valorA = strtol(stringA.c_str(), NULL, 2);
+            int valorB = strtol(stringB.c_str(), NULL, 2);
+
+            if (operacao == "sub") {
+                valorB = (~valorB) & 0x0F;
+            }
+
+            int somaProvisoria = valorA + valorB;
+
+            int resultadoFinal = somaProvisoria;
+            if (somaProvisoria > 15) {
+                int bitQueSobrou = (somaProvisoria >> 4) & 0x01;
+                int baseDe4Bits = somaProvisoria & 0x0F;
+                resultadoFinal = baseDe4Bits + bitQueSobrou;
+            }
+
+            digitalWrite(PINOS_LEDS[0], (resultadoFinal >> 0) & 0x01);
+            digitalWrite(PINOS_LEDS[1], (resultadoFinal >> 1) & 0x01); 
+            digitalWrite(PINOS_LEDS[2], (resultadoFinal >> 2) & 0x01); 
+            digitalWrite(PINOS_LEDS[3], (resultadoFinal >> 3) & 0x01);
+
+            Serial.print("Operando A (Decimal): ");
+            Serial.println(valorA);
+            
+            Serial.print("Operando B (Decimal): ");
+            Serial.println(valorB);
+            
+            Serial.print("Resultado Final (Decimal): ");
+            Serial.println(resultadoFinal);
+            
+            Serial.print("Estado dos LEDs (Binario): ");
+            Serial.print((resultadoFinal >> 3) & 0x01);
+            Serial.print((resultadoFinal >> 2) & 0x01);
+            Serial.print((resultadoFinal >> 1) & 0x01);
+            Serial.println((resultadoFinal >> 0) & 0x01);
+            Serial.println("---------------------------------");
+        }
+    }
 }
